20dfe30e960d98db2c259a355ecef96f43ca6021
Şahin Akkaya <sahin@sahinakkaya.dev>
2023-12-26 11:27:06 +0300

    Add custom configurations

---
+++ fzf.zsh
@@ -0,0 +1,11 @@
+# Setup fzf
+# ---------
+
+## for arch ##
+# Auto-completion
+# source "/usr/share/fzf/completion.zsh"
+# Key bindings
+# source "/usr/share/fzf/key-bindings.zsh"
+
+## for fedora ##
+source "/usr/share/fzf/shell/key-bindings.zsh"
