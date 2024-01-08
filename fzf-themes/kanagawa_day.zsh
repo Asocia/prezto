db6e9b98eb257deaed44948e381924e24e816897
Şahin Akkaya <sahin@sahinakkaya.dev>
2024-01-08 23:53:03 +0300

    Add fzf themes

---
+++ fzf-themes/kanagawa_day.zsh
@@ -0,0 +1,5 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+--color=fg:#545464,bg:#f2ecbc,hl:#cc6d00 \
+--color=fg+:#545464,bg+:#d5cea3,hl+:#cc6d00 \
+--color=info:#77713f,prompt:#5e857a,pointer:#5e857a \
+--color=marker:#6f894e,spinner:#6f894e,header:#6f894e"
---
+++ fzf-themes/kanagawa_night.zsh
@@ -0,0 +1,5 @@
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --color=fg:#C8C093,bg:#1F1F28,hl:#FFA066 \
+--color=fg+:#DCD7BA,bg+:#15161E,hl+:#FFA066 \
+--color=info:#C8C093,prompt:#7AA89F,pointer:#7AA89F \
+--color=marker:#98BB6C,spinner:#98BB6C,header:#98BB6C"
